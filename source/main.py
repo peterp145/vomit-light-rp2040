ed7498ebac32e14ef5d997583fc1f3b0c71cc1c7
peterp145 <peter@peterphelan.net>
2023-04-11 15:25:04 -0400

    wip

--- source/main.py
+++ source/main.py
@@ -1,10 +1,14 @@
 from machine import Pin
+from machine import SPI
 from time import sleep_ms
+from rgb_leds import RgbLeds
 
 # initialize
-print("Hello, Blinky!")
+print("Hello, Darla!")
 
 led = Pin("LED", Pin.OUT)
+# spi = SPI(0, 400_000)
+# rgb = RgbLeds(spi)
 
 # main loop
 while True:
--- source/rgb_leds.py
+++ source/rgb_leds.py
@@ -1,7 +1,28 @@
 ## imports ##
 from machine import SPI
 
+class PixelBuffer():
+
+    def __init__(self, length: int = 32, width: int = 1) -> None:
+        # dimensions of LED strip/array
+        self.length = length
+        self.width  = 1 #width
+
+        self._buffer    = bytearray(length*width*3)
+
+    def set_pixel(self, x_pos, y_pos, value) -> None:
+        pass
+
+    def get_pixel(self, x_pos, y_pos) -> bytearray:
+        return bytearray(3)
+    
+    def shift_pixels(self, x_shift, y_shift) -> bytearray:
+        return bytearray(1)
+
 class RgbLeds():
     def __init__(self, spi: SPI, length: int = 32) -> None:
         self._spi = spi
         self._length = length
+
+        self.buffer = PixelBuffer()
+
